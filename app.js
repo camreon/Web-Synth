3c37bea9b6eb803174ac76b849f2bc8a1841c833
camreon <cguthrie7@gatech.edu>
2014-05-10 20:54:27 -0400

    continuous playback, synth control effects, & volume controls

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 //
 // Each of the event handling methods has already been bound to the
 // custom events we have provided for you (event descriptions are provided below).
-// 
+//
 // @author Cameron Guthrie
 
-var context;
-var source;
-var buffer = null;
-
-var src = "assets/808/";        // base of the urls
-var drumNames = [ "bd",         // array of the name of each drum sample
-                  "sd", 
-                  "lt", 
-                  "mt",
-                  "ht", 
-                  "lc", 
-                  "mc", 
-                  "hc", 
-                  "rs", 
-                  "cl", 
-                  "ma", 
-                  "cp", 
-                  "cb", 
-                  "cy", 
-                  "oh", 
-                  "ch" ];  
-var noteFreqs = { 'C3' : 130.813,  // converts from note name to frequency
+var LENGTH = 16,                   // number of drum samples
+    FREQRATIO = 1.0594630943592,   // multiply frequency by ratio to step between notes
+    SRC = "assets/808/",           // base of the urls
+    DRUMNAMES = [ "bd", "sd", "lt", "mt", "ht", "lc", "mc", "hc",
+                  "rs", "cl", "ma", "cp", "cb", "cy", "oh", "ch" ], // array of the name of each drum sample
+    NOTEFREQS = { 'C3' : 130.813,  // converts from note name to frequency
                   'D3' : 146.832,
                   'E3' : 164.814,
                   'F3' : 174.614,
@@ -38,34 +22,50 @@ var noteFreqs = { 'C3' : 130.813,  // converts from note name to frequency
                   'F#3': 184.997,
                   'G#3': 207.652,
                   'A#3': 233.082 };
-var drumSamples = new Array();       // key-value pairs of sample name and sound object
-var length = 16;                     // number of drum samples
-var tempo = 128;                     // BPM
-var sixteenthNoteTime = 15 / tempo;  // length of a 16th note
-var isPlaying = false;               // continuous playback flag
-var freqRatio = 1.0594630943592;     // multiply frequency by ratio to step between notes
-var type = 2;                        // type of waveform for oscillator, defaults to 2
+
+var context,
+    source,
+    buffer = null,
+
+    drumSamples = new Array(),       // key-value pairs of sample name and sound object
+    sixteenthNoteTime = 15 / 128,    // default length of a 16th note
+    isPlaying = false,               // continuous playback flag
+    type = 2,                        // type of waveform for oscillator, defaults to 2
+    startTime,                       // start time of loop
+    endTime,                         // end time of loop
+    tune = 0,
+    filter,
+    accGainValue = 0,
+    decayValue = 0,
+    resValue = 0,
+    cutoff = 0,
+    envValue = 0,
+
+    trGain, tbGain, maGain;          // top level gain nodes
 
 
 function setupAudioGraph() {
     // setup your audio graph here
-    // this method is called automatically once the DOM loads
     try {
 		    window.AudioContext = window.AudioContext || window.webkitAudioContext;
 		    context = new AudioContext();
         // start the animation timer
         window.webkitRequestAnimationFrame(timer);
 
-        // setup sound objects
-        for (var i = 0; i < length; i++) {
-            var sample = new sound();
-            var name = drumNames[i];            // add sample name
-            sample.url = src + name + ".wav";   // add file URL
-            sample.gain = context.createGain(); // add gain node
-            sample.gain.gain.value = .5;        // set to default value
+        // setup drum sequencer
+        for (var i = 0; i < LENGTH; i++) {
+            var sample = new sound(SRC + DRUMNAMES[i] + ".wav", context.createGain());
             sample.load();
-            drumSamples[name] = sample;
+            drumSamples[DRUMNAMES[i]] = sample;
         }
+
+        // setup gain nodes
+        trGain = context.createGain();
+        trGain.gain.value = .5;
+        tbGain = context.createGain();
+        tbGain.gain.value = .5;
+        maGain = context.createGain();
+        maGain.gain.value = .5;
 
     } catch(e) {
         alert(e);
@@ -73,109 +73,107 @@ function setupAudioGraph() {
 }
 
 function timer() {
-    if (isPlaying)
-    {
-       // the timer method will be called back in sync with the refresh rate of your display
-       // (normally 60hz)
-       //
-       // you will need to look at context.currentTime to determine where you are in time
-       // and if you should schedule the next set of notes to play based on how much time
-       // has passed
-       console.log("we are playing");
-       console.log(context.currentTime);
-    } else {
-       // stop all the sounds that are currently playing
-      console.log("not playing");
-    }
- 
+    if (isPlaying && context.currentTime >= endTime)
+        playSequencers();
+
     // always schedule the next animation frame at the end of your callback
     window.webkitRequestAnimationFrame(timer);
- }
+}
 
-// Plays sounds from a new oscillator based on the note parameter converted 
-// to frequency. Starts at the time parameter and stops after a 16th beat. 
-function playSound(note, time, modifier, i) { 
+function playSequencers() {
+    startTime = context.currentTime;
+    endTime = startTime + 16 * sixteenthNoteTime; //end time of a single loop
+
+    // loop through note sequencer
+    for (var i = 0; i < LENGTH; i++) {
+        var note = window.noteSequencer.sequence.notes[i];
+        var modifier = window.noteSequencer.sequence.modifiers[i];
+        playSound(note, startTime + i * sixteenthNoteTime, modifier, i);
+    }
+
+    // loop through each drum sample sequence
+    for (property in drumSamples) {
+        var seq = window.drumSequencer.sequence[property];
+        // check which parts of the sequence are on
+        for (var i = 0; i < LENGTH; i++) {
+            if (seq[i])
+                drumSamples[property].play(startTime + i * sixteenthNoteTime);
+        }
+    }
+}
+
+// Plays sounds from a new oscillator based on the note parameter converted
+// to frequency. Starts at the time parameter and stops after a 16th beat.
+function playSound(note, time, modifier, i) {
     if (note != 0) {
         var now = context.currentTime;
         var osc = context.createOscillator(); // create new oscillator node on every play
-        osc.frequency.value = noteFreqs[note]; // set frequency based on note name
-        osc.type = type; 
+        osc.frequency.value = NOTEFREQS[note]; // set frequency based on note name
+        osc.type = type;
 
         var gainNode = context.createGain();
         gainNode.gain.value = .5; // default value
 
+        // adjust tune
+        if (tune)
+            osc.detune.value = Math.pow(2, 1/12) * tune;
+
         // update based on existing modifiers
         if (modifier) {
             if (modifier == "up")
-                osc.frequency.value *= freqRatio;
+                osc.frequency.value *= FREQRATIO;
             else if (modifier == "down")
-                osc.frequency.value /= freqRatio;
+                osc.frequency.value /= FREQRATIO;
             // set next note to current note
             if (modifier == "slide")
                 window.noteSequencer.sequence.notes[i + 1] = note;
-            // accent means turning volume controls on
+            // accent means turning volume controls on?
             if (modifier == "accent") {
-                gainNode.gain.value = 1;
+                gainNode.gain.value = accGainValue;
             }
         }
-        
-        osc.connect(gainNode);
-        gainNode.connect(context.destination);
-        osc.start(time); 
+
+        // filter settings
+        filter = context.createBiquadFilter();
+        filter.Q.value = resValue;
+        filter.frequency.value = cutoff; // TODO: test this
+        if (decayValue)
+            filter.gain.setTargetAtTime(0, time + decayValue, 1);
+        if (envValue)
+            filter.frequency.setTargetAtTime(0, time + envValue, 1);
+
+        // connect nodes
+        osc.connect(filter);
+        filter.connect(gainNode);
+        gainNode.connect(tbGain);
+        tbGain.connect(maGain);
+        maGain.connect(context.destination);
+
+        osc.start(time);
         osc.stop(time + sixteenthNoteTime);
     }
 }
 
 function onTempoChangeEvent(tempoEvent){
-    // save tempo in global var
-    tempo = tempoEvent.bpm;
+    sixteenthNoteTime = 15 / tempoEvent.bpm;
 }
 
 function onTransportControlEvent(transportEvent) {
     if (transportEvent.transportControl == "play") {
-        
-        // start playing the rhythm "now"
-        var startTime = context.currentTime;
-        var time = startTime + 16 * sixteenthNoteTime;
-        
-        // loop through note sequencer
-        for (var i = 0; i < length; i++) {
-            var note = window.noteSequencer.sequence.notes[i];
-            var modifier = window.noteSequencer.sequence.modifiers[i];
-            playSound(note, time + i * sixteenthNoteTime, modifier, i);
-        }
-
-        // loop through each drum sample sequence
-        for (property in drumSamples) {
-            var seq = window.drumSequencer.sequence[property];
-            // check which parts of the sequence are on
-            for (var i = 0; i < length; i++) {
-                if (seq[i]) 
-                    drumSamples[property].play(time + i * sixteenthNoteTime);
-            }
-        }
-
-    } else {
-        // stop everything
-        // for (var seq in window.drumSequencer.sequence) {
-        for (var sample in drumSamples) {
-            //sample.stop(); // err: undefined is not a function?
-        }
-        // }
-
-    }
+        isPlaying = true;
+        playSequencers();
+    } else
+        isPlaying = false;
 }
 
 function onFaderChangeEvent(faderEvent) {
     // get sound based on first 2 letters of fader name
-    var name = faderEvent.fader;
-    console.log(name);
-    name = name.substring(0,2);
-    // sound's gain = adjusted faderValue
-    drumSamples[name].gain.gain.value = faderEvent.faderValue / 100.0;
+    var name = faderEvent.fader.substring(0,2);
 
-    // add tr, tb, and master mix cases
-
+    if (name == "tr")      trGain.gain.value = faderEvent.faderValue / 100.0;
+    else if (name == "tb") tbGain.gain.value = faderEvent.faderValue / 100.0;
+    else if (name == "ma") maGain.gain.value = faderEvent.faderValue / 100.0;
+    else                   drumSamples[name].gain.gain.value = faderEvent.faderValue / 100.0;
 }
 
 function onFxParamChangeEvent(fxEvent) {
@@ -211,31 +209,40 @@ function onWaveformChangeEvent(waveformEvent) {
         type = 1;
     else
         type = 2;
-}    
-    
+}
+
 function onTuneChangeEvent(tuneEvent) {
-    console.log(tuneEvent.tuneValue);
+    tune = tuneEvent.tuneValue / 4.166666666;
 }
 
 function onFilterChangeEvent(filterEvent) {
-    // console.log(filterEvent.filterParamName);
-    // console.log(filterEvent.filterParamValue / 100.0);
-    // get sound based on first 2 letters of fader name
-    var name = filterEvent.filterParamName;
-    name = name.substring(0,2); 
+    var prefix = filterEvent.filterParamName.substring(0,2);
+    var name = filterEvent.filterParamName.substring(2);
+    // console.log(name);
 
-    // oscillator gain node value = filterEvent.filterParamValue / 100.0
+    if (prefix == "tb") {
+        if (name == "Cutoff")
+            cutoff = filterEvent.filterParamValue * 10.0;
+        else if (name == "Resonance")
+            resValue = filterEvent.filterParamValue * 10.0;
+        else if (name == "Env")
+            envValue = filterEvent.filterParamValue / 1000.0;
+        else if (name == "Decay")
+            decayValue = filterEvent.filterParamValue / 1000.0;
+        else if (name == "Accent")
+            accGainValue = filterEvent.filterParamValue / 100.0;
+    }
 }
 
 //  Custom Events
-//  
+//
 //  Each of the custom events is wrapped in an HTML5 Event.
 //
-//  Note regarding custom event values: 
+//  Note regarding custom event values:
 //  You are responsible for properly interpolating between the raw values provided
 //  by the user interface callbacks and values appropriate for the audio graph.
 //  For example, if you have an audio parameter that takes a value between 0 and 1
-//  you are responsible for computing that value based on the the UI value (which 
+//  you are responsible for computing that value based on the the UI value (which
 //  will normally be between 0 and 100)./
 //
 //  Event Descriptions
@@ -269,13 +276,13 @@ function onFilterChangeEvent(filterEvent) {
 //
 //  @event DrumSequencerEvent
 //  @description    Fired when the drum sequencer is interacted with
-//  @param  drumSequencePosition    a number between 1 and 16 that identifies the 
+//  @param  drumSequencePosition    a number between 1 and 16 that identifies the
 //  drum step sequence position activated
 //  @param  drumSequenceValue       possible values are 1 or 0
 //
 //  @event NoteSequencerEvent
 //  @description     Fired when the TB note sequencer is interacted wth
-//                   The note sequence will automatically be encoded 
+//                   The note sequence will automatically be encoded
 //                   by the time this event is dispatched
 //                   You can obtain the current note step from the global
 //                   window.lib.currentNoteStep property
@@ -286,7 +293,7 @@ function onFilterChangeEvent(filterEvent) {
 //  @description    Fired when the TB synth's waveform is changed
 //  @param  waveform    possible values are "saw" and "square"
 //
-//  @event  TuneChangeEvent 
+//  @event  TuneChangeEvent
 //  @description    Fired when the TB synth's "Tune" parameter is modified
 //  @param  tuneValue   number between 0 and 100
 //
@@ -294,9 +301,9 @@ function onFilterChangeEvent(filterEvent) {
 //  @description    Fired when the TB synth's filter parameters are modified
 //  @param  filterParamName     name of the filter parameter being modified
 //  @param  filterParamValue    value of the filter parameter between 0 and 100
-//      
+//
 
-document.addEventListener("TempoChangeEvent", onTempoChangeEvent);    
+document.addEventListener("TempoChangeEvent", onTempoChangeEvent);
 document.addEventListener("TransportControlEvent", onTransportControlEvent);
 document.addEventListener("FaderChangeEvent", onFaderChangeEvent);
 document.addEventListener("FxParamChangeEvent", onFxParamChangeEvent);
